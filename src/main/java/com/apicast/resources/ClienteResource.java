1457cc69fc74e11f6f8d5072c85dc8caa3d2c021
SimoneSalvi <simonebonemer@yahoo.com.br>
2021-11-09 14:44:32 -0300

    metodo update

--- src/main/java/com/apicast/resources/ClienteResource.java
+++ src/main/java/com/apicast/resources/ClienteResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,5 +53,11 @@ public class ClienteResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity <Cliente> update(@RequestBody Cliente cliente,@PathVariable Long id){
+		cliente = service.update(id,cliente);
+		return ResponseEntity.ok().body(cliente);
+	}
 
 }
--- src/main/java/com/apicast/service/ClienteService.java
+++ src/main/java/com/apicast/service/ClienteService.java
@@ -4,7 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.apicast.entities.Cliente;
 import com.apicast.repositories.ClienteRepository;
@@ -34,5 +38,20 @@ public class ClienteService {
 	//Delete
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+
+	//Update
+	public Cliente update(Long id, Cliente cliente) {
+		Cliente entity = repository.getOne(id);
+		updateData(entity,cliente);
+		return repository.save(entity);
+	}
+	
+	private void updateData(Cliente entity, Cliente cliente) {
+		entity.setCPF(cliente.getCPF());
+		entity.setDataN(cliente.getDataN());
+		entity.setEmail(cliente.getEmail());
+		entity.setNome(cliente.getNome());
+		entity.setSenha(cliente.getSenha());
 	}
 }
